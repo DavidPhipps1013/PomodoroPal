767bcbf80dc09f20738648eed0f6270cc3286341
DaveyP <davidphipps1013@gmail.com>
2026-02-01 22:39:25 -0500

    It works (sorta)
    CURRENTLY! No timer, just blocks out a single webpage that I designated that it should block out. Need to figure out timer logic. Need to refresh on html / js button logic.

--- Hello World Extension/scripts/content.js
+++ Hello World Extension/scripts/content.js
@@ -3,6 +3,8 @@ console.log("Content.js is running!")
 var img = document.createElement('img');
 img.src = chrome.runtime.getURL('images/overlay.png');
 
+img.style.width = '100%'
+img.style.height = '100%'
 img.style.position = 'fixed';
 img.style.top = '50%';
 img.style.left = '50%';
