4569635e914e28165fe295d50ed2ef25806f0e7e
DaveyP <davidphipps1013@gmail.com>
2026-02-01 21:24:49 -0500

    Makin progress?
    I truly don't know what or why is happening, but instead of the overlay being over the webpage, it's just in the little drop down box. Learning to change that now.

---
+++ Hello World Extension/scripts/content.js
@@ -0,0 +1,12 @@
+console.log("Content.js is running!")
+
+var img = document.createElement('img');
+img.src = chrome.runtime.getURL('images/overlay.png');
+
+img.style.position = 'fixed';
+img.style.top = '50%';
+img.style.left = '50%';
+img.style.transform = 'translate(-50%, -50%)';
+img.style.zIndex = '999999';
+
+document.body.appendChild(img);
