4ae88b3e36e265c2a999aaaddc5a1758e23b2940
DaveyP <davidphipps1013@gmail.com>
2026-02-02 21:21:28 -0500

    Pomodoro Pal V0.1
    Added a background to the chrome extension dropdown menu, and added a gif that sits on top of the image that blocks out websites. Also added a small animation I made in aseprite, but it's hard to see so I added a placeholder gif.

--- Hello World Extension/scripts/content.js
+++ Hello World Extension/scripts/content.js
@@ -1,14 +1,31 @@
-console.log("Content.js is running!")
+const blocker = document.createElement('div');
+blocker.style.position = 'fixed';
+blocker.style.top = '0';
+blocker.style.left = '0';
+blocker.style.width = '100vw';
+blocker.style.height = '100vh';
+blocker.style.zIndex = '999999';
 
-var img = document.createElement('img');
-img.src = chrome.runtime.getURL('images/overlay.png');
+const backGround = document.createElement('img');
+backGround.src = chrome.runtime.getURL('images/overlay.png');
+backGround.style.position = 'absolute';
+backGround.style.width = '100%';
+backGround.style.height = '100%';
+backGround.style.top = '0%';
+backGround.style.left = '0%';
+backGround.style.zIndex = '1';
 
-img.style.width = '100%'
-img.style.height = '100%'
-img.style.position = 'fixed';
-img.style.top = '50%';
-img.style.left = '50%';
-img.style.transform = 'translate(-50%, -50%)';
-img.style.zIndex = '999999';
+const animation = document.createElement('img');
+animation.src = chrome.runtime.getURL('images/parappa.gif');
+animation.style.position = 'absolute';
+animation.style.top = '50%';
+animation.style.left = '50%';
+animation.style.transform = 'translate(-50%, -50%)';
+animation.style.width = '100%';
+animation.style.height = '100%';
+animation.style.objectFit = 'contain';
+animation.style.zIndex = '2';
 
-document.body.appendChild(img);
+document.body.appendChild(blocker);
+blocker.appendChild(backGround);
+blocker.appendChild(animation);
--- Hello World Extension/scripts/timer.js
+++ Hello World Extension/scripts/timer.js
@@ -1,1 +1,12 @@
-console.log("AGHHHHH")
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("startButton").addEventListener("click", startPomodoro);
+    document.getElementById("endButton").addEventListener("click", endPomodoro);
+});
+
+function startPomodoro() {
+    alert("POMODORO STARTED!!");
+}
+
+function endPomodoro() {
+    alert("POMODORO ENDED!!");
+}
